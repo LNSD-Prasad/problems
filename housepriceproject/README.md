a137af44f9fb42f641674c0142ee5d897c096008
LNSD-Prasad <nagasaidurgaprasad223@gmail.com>
2026-03-29 21:25:07 +0530

    #Added house price prediction project files

---
+++ housepriceproject/README.md
@@ -0,0 +1,26 @@
+# 🏠 House Price Prediction using Linear Regression (Python)
+
+This project predicts house prices based on:
+- Area
+- Number of Bedrooms
+- Age of House
+- Distance from City
+
+It uses **Pandas** and **Scikit-learn LinearRegression**.
+
+---
+
+## 📂 Files in this project
+
+- `housepred.py` → Main Python program
+- `pricedata.csv` → Dataset used for training
+- `README.md` → Project documentation
+
+---
+
+## ⚙️ Requirements
+
+Install required libraries:
+
+```bash
+pip install pandas scikit-learn
---
+++ housepriceproject/housepred.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv("E:\problems\gitprojects\pricedata.csv")
+print(df)
+x=df[['Area','Bedrooms','Age','Distance']]
+y=df['Price']
+model=LinearRegression()
+model.fit(x,y)
+area=float(input('Enter area:'))
+bedrooms=int(input('Enter number of bedrooms:'))
+age=int(input('enter age of house:'))
+distance=float(input('Enter distance from city:'))
+user_input=[[area,bedrooms,age,distance]]
+predicted_price=model.predict(user_input)
+print('predicted House price:',predicted_price[0])
