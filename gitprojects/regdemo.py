020bf6ba4c1d034f5dafea849ecd31fffea7434c
LNSD-Prasad <nagasaidurgaprasad223@gmail.com>
2026-04-04 23:24:52 +0530

    #regexdemo

---
+++ gitprojects/regdemo.py
@@ -0,0 +1,15 @@
+import re 
+text='''John Doe, 25
+Jane Smith, 30
+email: user123@gmail.com
+email: test.user@yahoo.com
+phone: 9876543210
+phone: 91234-56789
+date: 12-05-2024
+date: 2024/05/12
+IP: 192.168.1.1
+URL: https://example.com '''
+pattern=re.compile(r'\d{10}|[a-z0-9A-Z_.]+@[a-z0-9A-Z]+\.[a-zA-Z]+')
+
+result=pattern.findall(text)
+print(result)
